31025221fec7a56fb51894aa54f34c24dd55ece7
3phraimY <ephraim.young@cuw.edu>
2025-04-03 14:56:36 -0500

    threaded count HW

--- Threaded Counting/a1.c
+++ Threaded Counting/a1.c
@@ -6,7 +6,7 @@
 // result: "thread id is %ld, and my turn is %d"
 
 const int k = 10;
-const int n = 1000;
+const int n = 101;
 
 pthread_t *threads;
 int *count;
@@ -18,7 +18,7 @@ void *threadCount(void *arg)
     int offset = *(int *)arg;
     for (int i = offset; i < offset + numbersPerThread; i++)
     {
-        count[i] = i;
+        count[i] = i + 1;
     }
     return NULL;
 }
@@ -28,16 +28,23 @@ int main()
     threads = malloc(k * sizeof(pthread_t));
     count = malloc(n * sizeof(int));
 
+    int remainingNumbers = n % k;
+
     for (int i = 0; i < k; i++)
     {
-        int *offset = malloc(sizeof(int));
-        *offset = i * numbersPerThread;
-        pthread_create(&threads[i], NULL, threadCount, offset);
+        int offset = i * numbersPerThread;
+        pthread_create(&threads[i], NULL, threadCount, &offset);
     }
 
     for (int i = 0; i < k; i++)
     {
         pthread_join(threads[i], NULL);
+    }
+
+    // write extra digits
+    for (int i = remainingNumbers; i > 0; i--)
+    {
+        count[n - i] = n - i + 1;
     }
 
     // Print the result
